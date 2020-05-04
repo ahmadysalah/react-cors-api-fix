d55cf6d245fa9d86a63d499d10b7ff2fbaf06d62
ahmadysalah <a.ahmad.salah@gmail.com>
2020-05-04 23:58:57 +0300

    add cors header midleware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ const {fitchGithub,fitchCors} = require("./controller");
 
 require('dotenv').config()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.set("port", process.env.PORT || 4002);
 
 app.get("/github-api-req",fitchGithub);
