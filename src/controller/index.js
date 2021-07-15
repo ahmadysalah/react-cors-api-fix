b48b6e6eb194af4657dca5972a69a375e6736e17
Alaa <aalzibda@trustangle.com>
2021-07-15 13:42:03 +0300

    update handle more than paeameters in query string

--- src/controller/index.js
+++ src/controller/index.js
@@ -6,7 +6,7 @@ const controller = {
     const { link } = req.query;
     axios
       .get(`${link}?access_token=${process.env.TOKEN}`)
-      .then((result) => res.json({status : 'Successfully', data : result.data }))
+      .then((result) => res.json({ status: 'Successfully', data: result.data }))
       .catch(() =>
         res.status(400).json({
           status: 400,
@@ -15,11 +15,18 @@ const controller = {
         })
       );
   },
-  fitchCors : (req, res) => {
-    const { link } = req.query;
+  fitchCors: (req, res) => {
+    // new update to handle more than parameter in query string 
+    const query = req.query;
+    const link = Object.keys(query)
+      .map((string, index) =>
+      (
+        `${index !== 0 ? '&':''}${string}=${query[string]}`
+      )).join('').replace('link=','')
+    console.log({ query,link })
     axios
       .get(`${link}`)
-      .then((result) => res.json({status : 'Successfully', data : result.data }))
+      .then((result) => res.json({ status: 'Successfully', data: result.data }))
       .catch(() =>
         res.status(400).json({
           status: 400,
@@ -29,5 +36,6 @@ const controller = {
       );
   },
 };
+
 
 module.exports = controller;
