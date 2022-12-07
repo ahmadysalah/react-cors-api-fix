9ac6f57b71e2090a1a7e82b07a37498774ab0a5f
ahmad_salah <a.ahmad.salah@gmail.com>
2022-12-07 11:13:59 +0200

    redeploy

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
 
-const {fitchGithub,fitchCors} = require("./controller");
+const { fitchGithub, fitchCors } = require("./controller");
 
 require('dotenv').config()
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 app.set("port", process.env.PORT || 4002);
+app.get("/", (req, res) => {
+  res.json("welcome to CORS");
+});
 
-app.get("/github-api-req",fitchGithub);
-app.get("/cors",fitchCors);
+app.get("/github-api-req", fitchGithub);
+app.get("/cors", fitchCors);
 
 app.use((req, res) => {
   res.status(404).json("wrong request");
